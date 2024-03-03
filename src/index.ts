5cbc561ffe657b03db734bee832940cb84516eef
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-03-03 14:30:33 +0530

    Create USERS TABLE

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-// Write a fuinction to create a users table in your database
+// Write a function to create a users table in your database.
+
+import { Client } from 'pg';
+
+const client = new Client({
+    connectionString: "Link of DB"
+})
+
+async function createUsersTable() {
+    await client.connect()
+    const result = await client.query(`
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+    `)
+    console.log(result);
+}
+createUsersTable();
